74aac1e30961f513802e1ab79f8f6cb1e52992a4
deny <denisson.graca3@gmail.com>
2023-07-01 02:22:42 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import FormLogin from "@/components/FormLogin/FormLogin";
+
+export default function Home() {
+  return (
+    <main className="bg-slate-200">
+      <div className="w-screen h-screen flex flex-1 items-center justify-center">
+        <FormLogin/>
+      </div>
+    </main>
+  )
+}
